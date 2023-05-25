c1fd14846ccc48e7292fdca62759c6e16833bdd1
Jaden Plugowsky <124389316+Jaden-Plugowsky@users.noreply.github.com>
2023-05-25 21:10:53 +0000

    template loaded

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Jaden Plugowsky All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Jaden Plugowsky
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict";
+
+function buttonOneClicked() {
+  //This function does XXXX
+  //Input through Textfields
+  const example = parseFloat(document.getElementById("example").value)
+
+  //Process
+  const answer = example
+
+  //Output
+  document.getElementById("answer").innerHTML =
+    "The answer is: " + answer.toFixed(2)
+}
