d777fcc7ef65890f642f666c46e4ef0d47e18e11
Jaden Plugowsky <124389316+Jaden-Plugowsky@users.noreply.github.com>
2023-05-26 02:28:25 +0000

    final website

--- js/script.js
+++ js/script.js
@@ -4,17 +4,73 @@
 // Created on: May 2023
 // This file contains the JS functions for index.html
 
-"use strict";
+"use strict"
 
-function buttonOneClicked() {
-  //This function does XXXX
+function calculatePressed() {
+  //This function takes three user-given triangle sides and calculates which type of triangle the sides make up: Isosceles, Scalene, Equilateral, or Right
   //Input through Textfields
-  const example = parseFloat(document.getElementById("example").value)
+  const sideA = parseFloat(document.getElementById("sideA").value)
+  const sideB = parseFloat(document.getElementById("sideB").value)
+  const sideC = parseFloat(document.getElementById("sideC").value)
+  const angleA = Math.round(
+    Math.acos((sideB ** 2 + sideC ** 2 - sideA ** 2) / (2 * sideB * sideC)) *
+      (180 / Math.PI)
+  )
+  const angleB = Math.round(
+    Math.acos((sideC ** 2 + sideA ** 2 - sideB ** 2) / (2 * sideC * sideA)) *
+      (180 / Math.PI)
+  )
+  const angleC = Math.round(
+    Math.acos((sideA ** 2 + sideB ** 2 - sideC ** 2) / (2 * sideA * sideB)) *
+      (180 / Math.PI)
+  )
+  const angleTotal = angleA + angleB + angleC
 
   //Process
-  const answer = example
+  if (angleA != 0 && angleB != 0 && angleC != 0) {
+    if (angleTotal == 180) {
+      // Calculates Right Triangle
+      if (angleA == 90 || angleB == 90 || angleC == 90) {
+        document.getElementById("answer").innerHTML =
+          "This triangle is a Right triangle."
+      }
+      // Calculates Equilateral
+      else if (angleA == angleC && angleB == angleC && angleA == angleB) {
+        document.getElementById("answer").innerHTML =
+          "This triangle is Equilateral."
+      }
+      // Calculates Isosceles
+      else if (angleA == angleC || angleB == angleC || angleA == angleB) {
+        document.getElementById("answer").innerHTML =
+          "This triangle is Isosceles."
+      }
+      // Calculates Scalene
+      else if (angleA != angleC && angleB != angleC && angleA == angleB) {
+        document.getElementById("answer").innerHTML =
+          "This triangle is Scalene."
+      } else {
+        document.getElementById("answer").innerHTML = "This is not a triangle."
+      }
+    } else {
+      document.getElementById("answer").innerHTML =
+        "This is not a triangle, as sides do not calculate angles which add up to 180 degrees total."
+    }
+  } else {
+    document.getElementById("answer").innerHTML = "This is not a triangle, as at least one of the angles is equal to 0."
+  }
 
   //Output
-  document.getElementById("answer").innerHTML =
-    "The answer is: " + answer.toFixed(2)
+  if (angleA != 0 && angleB != 0 && angleC != 0 && angleTotal == 180) {
+  document.getElementById("data").innerHTML =
+    "angleA: " +
+    angleA +
+    " angleB: " +
+    angleB +
+    " angleC: " +
+    angleC +
+    " Angles added together: " +
+    angleTotal
+  } else {
+    document.getElementById("data").innerHTML = ""
+  }
 }
